8b634bcf90f719745a503a3f913f2d56a375e086
Kshitiz <minakshitiz2005@gmail.com>
2024-03-18 12:15:11 +0530

    Solved the two sum question

--- python.py
+++ python.py
@@ -1,1 +1,8 @@
-print("Hello world")
+array = input()
+target = int(input())
+
+for i in range(len(array)):
+    for j in range(i+1, len(array)):
+        if array[i] + array[j] == target:
+            print(i, j)
+            break
